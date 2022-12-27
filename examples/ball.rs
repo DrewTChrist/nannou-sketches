8187b83bdf573e69e48a6d9f1eac2244e84e3b23
Andrew Christiansen <andrewtaylorchristiansen@gmail.com>
2022-12-27 15:51:46 -0800

    Update sketches

--- examples/ball.rs
+++ examples/ball.rs
@@ -4,6 +4,9 @@ fn main() {
     nannou::app(model).update(update).simple_window(view).run();
 }
 
+const RADIUS: f32 = 20.0;
+const FLOOR: f32 = 0.0;
+
 struct Model {
     x: f32,
     y: f32,
@@ -14,25 +17,23 @@ struct Model {
 fn model(_app: &App) -> Model {
     Model {
         x: 0.0,
-        y: 75.0,
+        y: 175.0,
         vx: 0.0,
         vy: 0.0,
     }
 }
 
 fn update(app: &App, model: &mut Model, _update: Update) {
-    let elapsed_time = app.duration.since_prev_update.as_secs_f32();
+    //let elapsed_time = app.duration.since_prev_update.as_secs_f32();
+    let elapsed_time = app.elapsed_frames() as f32 / 60.0;
 
-    // Apply gravity to the vertical velocity.
-    model.vy -= 9.8 * elapsed_time * 20.0;
+    model.vy -= 9.8 * elapsed_time;
 
-    // Update the position based on the velocity.
     model.x += model.vx * elapsed_time;
     model.y += model.vy * elapsed_time;
 
-    // If the ball hits the ground, bounce it back up with a reduced velocity.
-    if model.y < 0.0 {
-        model.y = 0.0;
+    if model.y <= FLOOR + RADIUS {
+        model.y = FLOOR + RADIUS;
         model.vy = -model.vy * 0.8;
     }
 }
@@ -41,12 +42,15 @@ fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
     draw.background().color(BLACK);
 
-    // Draw the ball.
     draw.ellipse()
         .x_y(model.x, model.y)
-        .radius(10.0)
+        .radius(RADIUS)
         .color(WHITE);
 
-    // Write to the window frame.
+    let start = pt2(app.window_rect().left(), FLOOR);
+    let end = pt2(app.window_rect().right(), FLOOR);
+
+    draw.line().start(start).end(end).weight(3.0).color(WHITE);
+
     draw.to_frame(app, &frame).unwrap();
 }
