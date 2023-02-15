62908802779c550078374278634dba6af440144d
Andrew Christiansen <andrewtaylorchristiansen@gmail.com>
2023-02-15 15:41:48 -0800

    Update sketches

--- examples/2023-02-14_flowfield.rs
+++ examples/2023-02-14_flowfield.rs
@@ -7,25 +7,136 @@ fn main() {
     nannou::app(model).update(update).run();
 }
 
+struct FlowField {
+    grid: Vec<Point>,
+    lines: Vec<Line>,
+    perlin: Perlin,
+    distance: f32,
+    length: f32,
+}
+
+impl FlowField {
+    fn new(
+        width: isize,
+        height: isize,
+        resolution: usize,
+        num_lines: usize,
+        distance: f32,
+        length: f32,
+        noise_t: f64,
+        noise_scale: f64,
+        coordinate_scale: f64,
+    ) -> Self {
+        let colors: Vec<Alpha<Rgb<f32>, f32>> = vec![0xa7d2cb, 0xf2d388, 0xc98474, 0x874c62]
+            .iter()
+            .map(|c| u32_to_srgba(*c, 0.7))
+            .collect();
+        let mut lines = Vec::new();
+        let mut grid = Vec::new();
+        let mut noise_xt = random_range(-5000.0, 5000.0);
+        let mut noise_yt = random_range(-5000.0, 5000.0);
+        let perlin = Perlin::new();
+        for i in (-width..width).step_by(resolution) {
+            for j in (-height..height).step_by(resolution) {
+                let noise = perlin.get([
+                    (noise_xt * noise_scale) + i as f64 * coordinate_scale,
+                    (noise_yt * noise_scale) + j as f64 * coordinate_scale,
+                ]);
+                let angle = map_range(noise, -1.0, 1.0, -2.0 * PI, 2.0 * PI);
+                grid.push(Point::new(i as f32, j as f32, angle));
+                noise_xt += noise_t;
+                noise_yt += noise_t;
+            }
+        }
+        let mut line_points = Vec::new();
+        for _ in 0..num_lines {
+            let x = random_range(-width as f32, width as f32);
+            let y = random_range(-height as f32, height as f32);
+            //let x = random_range(-100.0, 100.0);
+            //let y = random_range(-100.0, 100.0);
+            if !line_points.contains(&pt2(x, y)) {
+                line_points.push(pt2(x, y));
+                lines.push(Line::new(x, y, colors[random_range(0, 4)]));
+            }
+        }
+        Self {
+            grid,
+            lines,
+            perlin,
+            distance,
+            length,
+        }
+    }
+    fn update(&mut self) {
+        //for point in &mut self.grid {
+        //    for i in 0..self.lines.len() {
+        //        let line_point = self.lines[i].points[self.lines[i].points.len() - 1];
+        //        if point.location.distance(line_point) <= self.distance {
+        //            //let r = random_range(0.0, 10.0);
+        //            //let chance = if r >= 5.0 { 32.0 } else { 0.0 };
+        //            //let x = line_point.x + (point.angle + chance).cos() * 1.0;
+        //            //let y = line_point.y + (point.angle + chance).sin() * 1.0;
+        //            let x = line_point.x + point.angle.cos() * self.length;
+        //            let y = line_point.y + point.angle.sin() * self.length;
+        //            self.lines[i].points.push(pt2(x, y));
+        //            self.lines[i].update();
+        //        }
+        //    }
+        //}
+        for line in &mut self.lines {
+            let line_point = line.points[line.points.len() - 1];
+            let mut nearest = None;
+            let mut dist = 1000000000.0;
+            for i in 0..self.grid.len() {
+                let cur_dist = line_point.distance(self.grid[i].location);
+                if cur_dist <= dist {
+                    nearest = Some(&self.grid[i]);
+                    dist = cur_dist;
+                }
+            }
+            if let Some(n) = nearest {
+                let x = line_point.x + n.angle.cos() * self.length;
+                let y = line_point.y + n.angle.sin() * self.length;
+                line.points.push(pt2(x, y));
+                line.update();
+            }
+        }
+    }
+    fn draw_grid(&self, draw: &Draw) {
+        for point in &self.grid {
+            let x = point.location.x + point.angle.cos() * 5.0;
+            let y = point.location.y + point.angle.sin() * 5.0;
+            draw.line()
+                .start(point.location)
+                .end(pt2(x, y))
+                .weight(1.0)
+                .color(RED);
+        }
+    }
+    fn draw_lines(&self, draw: &Draw) {
+        for line in &self.lines {
+            line.draw(&draw);
+        }
+    }
+}
+
 struct Point {
     location: Vec2,
-    noise_xt: f64,
-    noise_yt: f64,
+    angle: f32,
 }
 
 impl Point {
-    fn new(x: f32, y: f32, noise_xt: f64, noise_yt: f64) -> Self {
+    fn new(x: f32, y: f32, angle: f32) -> Self {
         Self {
             location: pt2(x, y),
-            noise_xt,
-            noise_yt,
+            angle,
         }
     }
 }
 
 struct Line {
     points: Vec<Vec2>,
-    color: Alpha<Rgb<f32>, f32>
+    color: Alpha<Rgb<f32>, f32>,
 }
 
 impl Line {
@@ -44,131 +155,38 @@ impl Line {
             draw.line()
                 .start(self.points[i - 1])
                 .end(self.points[i])
-                .weight(10.0)
+                .weight(2.0)
                 .color(self.color);
-            }
+        }
     }
 }
 
 struct Model {
-    points: Vec<Point>,
-    perlin: Perlin,
-    lines: Vec<Line>,
+    field: FlowField,
+    grid_on: bool,
 }
-
-impl Model {
-    fn draw_grid(&self, draw: &Draw) {
-        for point in &self.points {
-            let noise = self
-                .perlin
-                .get([point.noise_xt * 0.5, point.noise_yt * 0.5]);
-            let angle = map_range(noise, -1.0, 1.0, -2.0 * PI, 2.0 * PI);
-            let x = point.location.x + angle.cos() * 5.0;
-            let y = point.location.y + angle.sin() * 5.0;
-            draw.line()
-                .start(point.location)
-                .end(pt2(x, y))
-                .weight(1.0)
-                .color(RED);
-            }
-    }
-    fn draw_lines(&self, draw: &Draw) {
-        for line in &self.lines {
-            line.draw(&draw);
-        }
-    }
-    fn update_lines(&mut self) {
-        for line in &mut self.lines {
-            line.update();
-        }
-    }
-    fn update_grid_noise(&mut self) {
-        for point in &mut self.points {
-            point.noise_xt += 0.005;
-            point.noise_yt += 0.005;
-        }
-    }
-}
-
-fn random_edge(w: f32, h: f32) -> Vec2 {
-    let rn = random_range(0.0, 100.0);
-    if rn <= 25.0 {
-        // top
-        pt2(random_range(-w, w), h)
-    } else if rn <= 50.0 {
-        // right
-        pt2(w, random_range(-h, h))
-    } else if rn <= 75.0 {
-        // bottom
-        pt2(random_range(-w, w), -h)
-    } else if rn <= 100.0 {
-        // left
-        pt2(-w, random_range(-h, h))
-    } else {
-        pt2(0.0, 0.0)
-    }
-}
-
-const STEP_BY: usize = 10;
-const NOISE_T: f64 = 0.0005;
-const NUM_LINES: usize = 500;
-const DIST: f32 = 15.0;
 
 fn model(app: &App) -> Model {
-    let _window_id = app.new_window().size(600, 600).view(view).build().unwrap();
-    let mut points = Vec::new();
-    let mut noise_xt = random_range(-500.0, 500.0);
-    let mut noise_yt = random_range(-500.0, 500.0);
-    for i in (-250..250).step_by(STEP_BY) {
-        for j in (-250..250).step_by(STEP_BY) {
-            points.push(Point::new(
-                i as f32,
-                j as f32,
-                noise_xt + i as f64 * 0.01,
-                noise_yt + j as f64 * 0.01,
-            ));
-            noise_xt += NOISE_T;
-            noise_yt += NOISE_T;
-        }
-    }
-    let perlin = Perlin::new();
-    let colors: Vec<Alpha<Rgb<f32>, f32>> = vec![0xa7d2cb, 0xf2d388, 0xc98474, 0x874c62]
-        .iter()
-        .map(|c| u32_to_srgba(*c, 0.7))
-        .collect();
-    let mut lines = Vec::new();
-    for _ in 0..NUM_LINES {
-        let x = random_range(-25.0, 25.0);
-        let y = random_range(-25.0, 25.0);
-        lines.push(Line::new(x, y, colors[random_range(0, 4)]));
-        //lines.push(Line::new(0.0, 0.0, colors[random_range(0, 4)]));
-    }
+    let _window_id = app
+        .new_window()
+        .size(600, 600)
+        .key_pressed(key_pressed)
+        .key_released(key_released)
+        .view(view)
+        .build()
+        .unwrap();
+    //let field = FlowField::new(300, 300, 3, 1200, 5.0, 5.0, 0.003, 0.001, 0.005);
+    let field = FlowField::new(300, 300, 3, 500, 5.0, 1.0, 0.003, 0.001, 0.0025);
     Model {
-        points,
-        perlin,
-        lines,
+        field,
+        grid_on: false,
     }
 }
 
 fn update(_app: &App, model: &mut Model, _update: Update) {
-    //model.update_grid_noise();
-    for point in &mut model.points {
-        for i in 0..model.lines.len() {
-            let line_point = model.lines[i].points[model.lines[i].points.len() - 1];
-            if point.location.distance(line_point) < DIST {
-                let noise = model.perlin.get([
-                    point.noise_xt + i as f64 * 0.025,
-                    point.noise_yt + i as f64 * 0.025,
-                ]);
-                let angle = map_range(noise, -1.0, 1.0, -2.0 * PI, 2.0 * PI);
-                let x = line_point.x + angle.cos() * 0.25;
-                let y = line_point.y + angle.sin() * 0.25;
-                model.lines[i].points.push(pt2(x, y));
-                model.lines[i].update();
-            }
-        }
-    }
-    //model.update_lines();
+    //if _app.elapsed_frames() < 5 {
+    model.field.update();
+    //}
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
@@ -176,7 +194,21 @@ fn view(app: &App, model: &Model, frame: Frame) {
     if app.elapsed_frames() == 1 {
         draw.background().color(BEIGE);
     }
-    //model.draw_grid(&draw);
-    model.draw_lines(&draw);
+    model.field.draw_lines(&draw);
+    if model.grid_on {
+        model.field.draw_grid(&draw);
+    }
     draw.to_frame(app, &frame).unwrap();
+}
+
+fn key_pressed(_app: &App, model: &mut Model, key: Key) {
+    if let Key::A = key {
+        model.grid_on = true;
+    }
+}
+
+fn key_released(_app: &App, model: &mut Model, key: Key) {
+    if let Key::A = key {
+        model.grid_on = false;
+    }
 }
