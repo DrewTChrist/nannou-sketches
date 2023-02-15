6251fe105371f1f74971ec385a99f692569023cd
Andrew Christiansen <andrewtaylorchristiansen@gmail.com>
2023-02-15 00:08:06 -0800

    Update sketches

--- examples/2023-02-14_flowfield.rs
+++ examples/2023-02-14_flowfield.rs
@@ -1,5 +1,7 @@
+use nannou::color::Alpha;
 use nannou::noise::{NoiseFn, Perlin};
 use nannou::prelude::*;
+use nannou_sketches::utilities::u32_to_srgba;
 
 fn main() {
     nannou::app(model).update(update).run();
@@ -23,16 +25,17 @@ impl Point {
 
 struct Line {
     points: Vec<Vec2>,
+    color: Alpha<Rgb<f32>, f32>
 }
 
 impl Line {
-    fn new(x: f32, y: f32) -> Self {
+    fn new(x: f32, y: f32, color: Alpha<Rgb<f32>, f32>) -> Self {
         let mut points = Vec::new();
         points.push(pt2(x, y));
-        Self { points }
+        Self { points, color }
     }
     fn update(&mut self) {
-        if self.points.len() > 2 {
+        if self.points.len() > 5 {
             self.points.remove(0);
         }
     }
@@ -41,9 +44,9 @@ impl Line {
             draw.line()
                 .start(self.points[i - 1])
                 .end(self.points[i])
-                .weight(2.0)
-                .color(WHITE);
-        }
+                .weight(10.0)
+                .color(self.color);
+            }
     }
 }
 
@@ -60,14 +63,14 @@ impl Model {
                 .perlin
                 .get([point.noise_xt * 0.5, point.noise_yt * 0.5]);
             let angle = map_range(noise, -1.0, 1.0, -2.0 * PI, 2.0 * PI);
-            let x = point.location.x + angle.cos() * 10.0;
-            let y = point.location.y + angle.sin() * 10.0;
+            let x = point.location.x + angle.cos() * 5.0;
+            let y = point.location.y + angle.sin() * 5.0;
             draw.line()
                 .start(point.location)
                 .end(pt2(x, y))
-                .weight(2.0)
-                .color(WHITE);
-        }
+                .weight(1.0)
+                .color(RED);
+            }
     }
     fn draw_lines(&self, draw: &Draw) {
         for line in &self.lines {
@@ -77,6 +80,12 @@ impl Model {
     fn update_lines(&mut self) {
         for line in &mut self.lines {
             line.update();
+        }
+    }
+    fn update_grid_noise(&mut self) {
+        for point in &mut self.points {
+            point.noise_xt += 0.005;
+            point.noise_yt += 0.005;
         }
     }
 }
@@ -102,8 +111,8 @@ fn random_edge(w: f32, h: f32) -> Vec2 {
 
 const STEP_BY: usize = 10;
 const NOISE_T: f64 = 0.0005;
-const NUM_LINES: usize = 100;
-const DIST: f32 = 10.0;
+const NUM_LINES: usize = 500;
+const DIST: f32 = 15.0;
 
 fn model(app: &App) -> Model {
     let _window_id = app.new_window().size(600, 600).view(view).build().unwrap();
@@ -123,12 +132,16 @@ fn model(app: &App) -> Model {
         }
     }
     let perlin = Perlin::new();
+    let colors: Vec<Alpha<Rgb<f32>, f32>> = vec![0xa7d2cb, 0xf2d388, 0xc98474, 0x874c62]
+        .iter()
+        .map(|c| u32_to_srgba(*c, 0.7))
+        .collect();
     let mut lines = Vec::new();
     for _ in 0..NUM_LINES {
-        //let edge = random_edge(250.0, 250.0);
-        let x = random_range(-250.0, 250.0);
-        let y = random_range(-250.0, 250.0);
-        lines.push(Line::new(x, y));
+        let x = random_range(-25.0, 25.0);
+        let y = random_range(-25.0, 25.0);
+        lines.push(Line::new(x, y, colors[random_range(0, 4)]));
+        //lines.push(Line::new(0.0, 0.0, colors[random_range(0, 4)]));
     }
     Model {
         points,
@@ -138,28 +151,31 @@ fn model(app: &App) -> Model {
 }
 
 fn update(_app: &App, model: &mut Model, _update: Update) {
-    //for point in &mut model.points {
-    //    point.noise_xt += 0.005;
-    //    point.noise_yt += 0.005;
-    //}
+    //model.update_grid_noise();
     for point in &mut model.points {
         for i in 0..model.lines.len() {
-            let lpoint = model.lines[i].points[model.lines[i].points.len() - 1];
-            if point.location.distance(lpoint) < DIST {
-                let noise = model.perlin.get([point.noise_xt, point.noise_yt]);
+            let line_point = model.lines[i].points[model.lines[i].points.len() - 1];
+            if point.location.distance(line_point) < DIST {
+                let noise = model.perlin.get([
+                    point.noise_xt + i as f64 * 0.025,
+                    point.noise_yt + i as f64 * 0.025,
+                ]);
                 let angle = map_range(noise, -1.0, 1.0, -2.0 * PI, 2.0 * PI);
-                let x = lpoint.x + angle.cos() * 1.0;
-                let y = lpoint.y + angle.sin() * 1.0;
+                let x = line_point.x + angle.cos() * 0.25;
+                let y = line_point.y + angle.sin() * 0.25;
                 model.lines[i].points.push(pt2(x, y));
+                model.lines[i].update();
             }
         }
     }
-    model.update_lines();
+    //model.update_lines();
 }
 
 fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
-    //draw.background().color(BLACK);
+    if app.elapsed_frames() == 1 {
+        draw.background().color(BEIGE);
+    }
     //model.draw_grid(&draw);
     model.draw_lines(&draw);
     draw.to_frame(app, &frame).unwrap();
